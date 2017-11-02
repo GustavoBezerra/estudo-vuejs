e6a8b35b43f4f08853ef13883acea868f88a3f57
Gustavo de Souza Bezerra <gustavo.bezerra@hotmail.com>
2017-11-02 18:49:14 -0200

    Initial commit

---
+++ alurapic/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{titulo}}</h1>
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo"/>
+      </li>
+    </ul>    
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      titulo: 'Alurapic',
+      fotos: [
+        {
+          url: 'https://i.ytimg.com/vi/8qA4UOLnC5A/maxresdefault.jpg',
+          titulo: 'Cachorro'
+        },
+        {
+          url: 'https://i.ytimg.com/vi/8qA4UOLnC5A/maxresdefault.jpg',
+          titulo: 'Cachorrão'
+        }
+      ]
+    }      
+  }
+}
+</script>
+
+<style>
+
+</style>
