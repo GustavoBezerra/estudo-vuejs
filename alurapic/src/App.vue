7169025a365396af712558374cfd7f4e3ed3930f
Gustavo Bezerra <gustavo.bezerra@hotmail.com>
2017-11-18 12:03:44 -0200

    Fim da aula 6

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,14 +1,31 @@
 
 <template>
   <div class="corpo">
+
+    <meu-menu :rotas="routes"/>
+
+    <transition name="pagina">
       <router-view></router-view>
+    </transition>
+    
   </div>
 </template>
 
 <script>
 
+import {routes} from './routes';
+import Menu from './components/shared/menu/Menu.vue';
+
 export default {
- 
+
+  components: {
+    'meu-menu' : Menu
+  },
+  data(){
+    return {
+      routes
+    }
+  }
 }
 </script>
 
@@ -17,5 +34,13 @@ export default {
     font-family: Helvetica, sans-serif;
     width: 96%;
     margin: 0 auto;
+  }
+
+  .pagina-enter, .pagina-leave-active{
+      opacity: 0;
+  }
+
+  .pagina-enter-active, .pagina-leave-active{
+      transition: opacity .4s;
   }
 </style>
